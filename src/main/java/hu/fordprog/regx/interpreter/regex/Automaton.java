af2265991fc360c729b0dc3f1df331520deee086
marianna-szabo-deik <szabo.marica@gmail.com>
2016-12-12 17:24:08 +0100

    Closure automaton creation added

--- src/main/java/hu/fordprog/regx/interpreter/regex/Automaton.java
+++ src/main/java/hu/fordprog/regx/interpreter/regex/Automaton.java
@@ -1,11 +1,9 @@
 package hu.fordprog.regx.interpreter.regex;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.stream.Stream;
 
 public class Automaton {
 
@@ -17,6 +15,7 @@ public class Automaton {
 
   public Automaton() {
     stateTransitionTable = new ArrayList<>();
+    acceptStates = new HashSet<>();
   }
 
   public List<StateTransition> getStateTransitionTable() {
--- src/main/java/hu/fordprog/regx/interpreter/regex/ClosureTerm.java
+++ src/main/java/hu/fordprog/regx/interpreter/regex/ClosureTerm.java
@@ -5,6 +5,47 @@ public class ClosureTerm extends Term {
     super(child);
   }
 
+  @Override
+  public Automaton makeAutomaton(){
+    Automaton closureAutomaton = new Automaton();
+
+    Automaton childAutomaton = this.makeAutomaton();
+
+    Automaton shiftedAutomaton = childAutomaton.getShiftedAutomaton(1);
+
+    closureAutomaton.setStartState(1);
+
+    //All original transitions stay in the new automaton
+    closureAutomaton.setStateTransitionTable(shiftedAutomaton.getStateTransitionTable());
+
+    //The previous acc states are linked to the previous start state
+    for(Integer acc : shiftedAutomaton.getAcceptStates()){
+
+      closureAutomaton
+          .addNewStateTransition(acc, "\\", shiftedAutomaton.getStartState());
+
+    }
+
+    //the new start state is linked to the previous start state
+    closureAutomaton.addNewStateTransition(1, "\\", shiftedAutomaton.getStartState());
+
+    Integer newAccState = closureAutomaton.getNextIdForNewState();
+
+    closureAutomaton.addNewAcceptState(newAccState);
+
+    //link the previous acc states to the new acc state
+    for(Integer acc : shiftedAutomaton.getAcceptStates()){
+
+      closureAutomaton
+          .addNewStateTransition(acc, "\\", newAccState);
+    }
+
+    //link the new start state to the new acc state
+    closureAutomaton.addNewStateTransition(1, "\\", newAccState);
+
+    return closureAutomaton;
+  }
+
   public String asText() {
     return super.asText() + "*";
   }
