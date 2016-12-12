b339c4c4695a6e3c18115939447ebe520dae2c97
marianna-szabo-deik <szabo.marica@gmail.com>
2016-12-12 14:39:56 +0100

    StateTransition class implemented

---
+++ src/main/java/hu/fordprog/regx/interpreter/regex/StateTransition.java
@@ -0,0 +1,39 @@
+package hu.fordprog.regx.interpreter.regex;
+
+public class StateTransition {
+  private Integer from;
+
+  private String with;
+
+  private Integer to;
+
+  public StateTransition(Integer from, String with, Integer to) {
+    this.from = from;
+    this.with = with;
+    this.to = to;
+  }
+
+  public Integer getFrom() {
+    return from;
+  }
+
+  public void setFrom(Integer from) {
+    this.from = from;
+  }
+
+  public String getWith() {
+    return with;
+  }
+
+  public void setWith(String with) {
+    this.with = with;
+  }
+
+  public Integer getTo() {
+    return to;
+  }
+
+  public void setTo(Integer to) {
+    this.to = to;
+  }
+}
